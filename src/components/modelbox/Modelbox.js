b1944be9d8aac0d0cea4f02faebe8b4724ba784e
akkuebler <91547040+akkuebler@users.noreply.github.com>
2023-10-13 09:53:45 +0200

    solved errors

--- src/components/modelbox/Modelbox.js
+++ src/components/modelbox/Modelbox.js
@@ -8,15 +8,15 @@ const ModelBox = ({ setAdopters, setAwareFarmers }) => {
     };
 
     return (
-        <div style={}>
+        <div>
             <h2>The Model</h2>
-            <div style={}>
+            <div>
                 <Dropdown label="Frequency Direct Ad:" options={['Option1', 'Option2']} />
                 <Dropdown label="Type Direct Ad:" options={['Option1', 'Option2']} />
                 <Dropdown label="Frequency Chief Training:" options={['Option1', 'Option2']} />
                 <TextInput label="Number of Ticks:" />
             </div>
-            <div style={}>
+            <div>
                 <Button label="Start Model without NetLogo GUI" onClick={() => runModel(false)} variant="solid-orange"/>
                 <Button label="Start Model with NetLogo GUI" onClick={() => runModel(true)} variant="solid-orange"/>
             </div>
