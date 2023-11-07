dcc0c2b76d3d2df2ad9f40aa310249d9c6b7a86e
hanuta27 <hannah.rohe@hispeed.ch>
2023-11-07 16:42:44 +0100

    add scrollbar to table

--- src/components/resultboxOptimizer/ResultboxOptimizer.js
+++ src/components/resultboxOptimizer/ResultboxOptimizer.js
@@ -7,37 +7,39 @@ const ResultboxOptimizer = ({ optimizationResults }) => {
   return (
     <div className='table-container'>
       <h3>Optimization Results</h3>
-      <table className='table-with-lines'>
-        <thead>
-          <tr>
-            <th>Direct Ad Type</th>
-            <th>Direct Ad Frequency</th>
-            <th>Training Chiefs Frequency</th>
-            <th>Direct Ad Number of Villages</th>
-            <th>Number of trained CHiefs</th>
-            <th>Avg Adopters</th>
-            <th>Number of direct Ads</th>
-            <th>Number of Chief Trainings</th>
-            <th>Total Costs</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-          {optimizationResults.slice().reverse().map((result, index) => (
-            <tr key={index} className={index === 0 ? "highlighted-row" : ""}>
-              <td>{result.directAdType}</td>
-              <td>{result.directAdFrequency}</td>
-              <td>{result.trainChiefsFrequency}</td>
-              <td>{result.directAdNrOfVillages}</td>
-              <td>{result.trainChiefsNumber}</td>
-              <td>{result.avgAdopters}</td>
-              <td>{result.nrOfDirectAds}</td>
-              <td>{result.nrOfChiefTrainings}</td>
-              <td>{result.totalCost}</td>
+      <div className='table-scroll'>
+        <table className='table-with-lines'>
+          <thead>
+            <tr>
+              <th>Direct Ad Type</th>
+              <th>Direct Ad Frequency</th>
+              <th>Training Chiefs Frequency</th>
+              <th>Direct Ad Number of Villages</th>
+              <th>Number of trained CHiefs</th>
+              <th>Avg Adopters</th>
+              <th>Number of direct Ads</th>
+              <th>Number of Chief Trainings</th>
+              <th>Total Costs</th>
+              
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {optimizationResults.slice().reverse().map((result, index) => (
+              <tr key={index} className={index === 0 ? "highlighted-row" : ""}>
+                <td>{result.directAdType}</td>
+                <td>{result.directAdFrequency}</td>
+                <td>{result.trainChiefsFrequency}</td>
+                <td>{result.directAdNrOfVillages}</td>
+                <td>{result.trainChiefsNumber}</td>
+                <td>{result.avgAdopters}</td>
+                <td>{result.nrOfDirectAds}</td>
+                <td>{result.nrOfChiefTrainings}</td>
+                <td>{result.totalCost}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
