600784ef542041ee9850122f5615b80660c2dca0
Joingl <joel.inglin@uzh.ch>
2024-01-22 15:08:58 +0100

    adjusted wording for ToT Influence

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const App = () => {
     };
 
     const PARAM_MAPPING = {
-        "trainChiefInfluence": "ToT Influence",
+        "trainChiefInfluence": "ToT Adoption Probability (%)",
         "nrDefaultFriendsInterVillage": "Number of Friends Inter-Village",
         "avgIntraVillageInteractionFrequency": "Intra-Village Interaction Frequency (days)",
         "avgInterVillageInteractionFrequency": "Inter-Village Interaction Frequency (days)",
@@ -72,7 +72,7 @@ const App = () => {
         "avgIntraVillageInteractionFrequency": "Average number of days between intra-village interactions initiated by an agent.",
         "avgInterVillageInteractionFrequency": "Average number of days between inter-village interactions initiated by an agent​.",
         "avgChiefFarmerMeetingFrequency": "Average number of days between farmgroup meetings.",
-        "trainChiefInfluence": "Measures the impact of ToTs (Training of Trainers) on the attitude of Chiefs regarding the innovation. E.g. 1.88 means a chief has a 88% chance to be an adopter after they received a training.",
+        "trainChiefInfluence": "Average probability of a chief adopting the innovation after being part of a Training of Trainers intervention (ToT).",
     };
 
     const initialParameters = Object.keys(PARAM_MAPPING).reduce((obj, key) => {
